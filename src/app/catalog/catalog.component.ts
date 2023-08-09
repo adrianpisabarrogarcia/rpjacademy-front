0ddbab2dbfca742a9ac01f4d9108f9d04102445f
Adrián Pisabarro <apisabarro@teknei.com>
2023-08-09 02:05:54 +0200

    💄 Catalog

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -26,32 +26,50 @@ export class CatalogComponent {
       isAvailable: true,
       category: 'WEB'
     },{
-      id: 3,
+      id: 4,
       name: 'Angular',
       description: 'Angular is a platform for building mobile and desktop web applications.',
       price: 200,
       imageUrl: 'https://repository-images.githubusercontent.com/24195339/87018c00-694b-11e9-8b5f-c34826306d36',
       isAvailable: true,
       category: 'WEB'
-    },
-    {
-      id: 4,
-      name: 'React',
-      description: 'React is a JavaScript library for building user interfaces.',
-      price: 150,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
+    },{
+      id: 5,
+      name: 'Angular',
+      description: 'Angular is a platform for building mobile and desktop web applications.',
+      price: 200,
+      imageUrl: 'https://repository-images.githubusercontent.com/24195339/87018c00-694b-11e9-8b5f-c34826306d36',
       isAvailable: true,
       category: 'WEB'
-    },
-    {
-      id: 5,
-      name: 'React',
-      description: 'React is a JavaScript library for building user interfaces.',
-      price: 150,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
+    },{
+      id: 6,
+      name: 'Angular',
+      description: 'Angular is a platform for building mobile and desktop web applications.',
+      price: 200,
+      imageUrl: 'https://repository-images.githubusercontent.com/24195339/87018c00-694b-11e9-8b5f-c34826306d36',
+      isAvailable: true,
+      category: 'WEB'
+    },{
+      id: 7,
+      name: 'Angular',
+      description: 'Angular is a platform for building mobile and desktop web applications.',
+      price: 200,
+      imageUrl: 'https://repository-images.githubusercontent.com/24195339/87018c00-694b-11e9-8b5f-c34826306d36',
+      isAvailable: true,
+      category: 'WEB'
+    },{
+      id: 8,
+      name: 'Angular',
+      description: 'Angular is a platform for building mobile and desktop web applications.',
+      price: 200,
+      imageUrl: 'https://repository-images.githubusercontent.com/24195339/87018c00-694b-11e9-8b5f-c34826306d36',
       isAvailable: true,
       category: 'WEB'
     }
   ];
+
+  enterClick() {
+    alert('Enter was pressed');
+  }
 
 }
