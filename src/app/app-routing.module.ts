86b80818fb1fca4ca60eb4a9ee04b4f4ac2ed568
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2023-08-23 23:33:16 +0200

    🛠️ Routes created & Course component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CourseComponent } from './course/course.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CatalogComponent },
+  { path: 'course/:id', component: CourseComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { CatalogComponent } from './catalog/catalog.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    CatalogComponent
+    CatalogComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
